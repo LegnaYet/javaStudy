49faf1b8e6779ccad3609dc33fe1ff43a84eb413
yechenhao <yechenhao>
2020-11-06 15:46:07 +0800

    第一阶段mybatis学习

---
+++ season1/IPersistence_Test/src/main/java/com/legna/mybatis/user/pojo/User.java
@@ -0,0 +1,23 @@
+package com.legna.mybatis.user.pojo;
+
+public class User {
+    private Long id;
+    private String username;
+    private String birthday;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
